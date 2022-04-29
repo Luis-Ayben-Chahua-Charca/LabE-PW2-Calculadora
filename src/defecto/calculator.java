97be5519e895ec00acbbc1826cda5ba53858aeb2
Luis fernando chambi orosco <lchambio@unsa.edu.pe>
2022-04-29 12:27:03 -0500

    agragando duda xd

--- src/defecto/calculator.java
+++ src/defecto/calculator.java
@@ -2,5 +2,6 @@ package defecto;
 public class calculator{
     public static void main(String args[]){
         System.out.print("calculadora ps mi king");
+        System.out.print("no entideno de caldular que es add y mod xd");
     }
 }
