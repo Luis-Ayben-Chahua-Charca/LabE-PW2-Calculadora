47da9fcf292c84753dac38a7696c26af704ff8d1
Luis fernando chambi orosco <lchambio@unsa.edu.pe>
2022-04-29 15:54:48 -0500

    agregando gui y agregacion suma

--- src/defecto/calculator.java
+++ src/defecto/calculator.java
@@ -1,7 +1,101 @@
 package defecto;
-public class calculator{
-    public static void main(String args[]){
-        System.out.print("calculadora ps mi king");
-        System.out.print("que fue mi king nose que son ese add mod xd");
+import java.awt.Color;
+import java.awt.Container;
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.*;
+
+
+public class calculator extends JFrame implements ActionListener {
+    
+  private JButton btnsuma,btnresta,btnmulti,btndiv,btnlimpiar,btnmod,btnsalir;
+ private JTextField valor1,valor2,camporesultado;
+ JLabel label1,label2,label3;
+  
+ public void Controles(){
+   
+      Container contenedor = getContentPane();
+      contenedor.setLayout( new FlowLayout());
+   label1 = new JLabel("Numero 1");
+      contenedor.add(label1);
+      valor1 = new JTextField(10);
+      contenedor.add(valor1);
+     label2 = new JLabel("Numero 2");
+      contenedor.add(label2);
+      valor2 = new JTextField(10);
+      contenedor.add(valor2);
+      btnsuma = new JButton("+");
+      contenedor.add(btnsuma);
+      btnsuma.addActionListener(this);
+      btnresta = new JButton("-");
+      contenedor.add(btnresta);
+      btnresta.addActionListener(this);
+      btnmulti = new JButton("x");
+      contenedor.add(btnmulti);
+      btnmulti.addActionListener(this);
+      btndiv = new JButton("/");
+      btndiv.addActionListener(this);
+      contenedor.add(btndiv);
+      btnmod=new JButton("mod");
+      btnmod.addActionListener(this);
+      contenedor.add(btnmod);
+      camporesultado = new JTextField(10);
+      contenedor.add(camporesultado);
+      btnlimpiar = new JButton("Limpiar");
+      contenedor.add(btnlimpiar);
+      btnlimpiar.addActionListener(this);
+      btnsalir=new JButton("Salir");
+      btnsalir.setBackground(Color.red);
+      contenedor.add(btnsalir);
+      btnsalir.addActionListener(this);
+ }
+  
+ public calculator(){
+      super("Calculadora");
+      Controles();
+      setSize(380,200);
+      setLocationRelativeTo(null);
+      setVisible(true);
+ }
+  
+ public void actionPerformed(ActionEvent e) {
+     double  num1,num2,resu;
+     String resultado;
+        if (e.getSource()==btnsuma) {
+         if(valor1.getText().equals("") || valor2.getText().equals(""))
+         {
+          JOptionPane.showMessageDialog(null, "Los datos no están completos",
+          "Error",JOptionPane.WARNING_MESSAGE); 
+         }
+         else{
+          num1=Double.parseDouble(valor1.getText());
+          num2=Double.parseDouble(valor2.getText());
+          resu=num1+num2;
+          resultado=String.valueOf(resu);
+          camporesultado.setText(resultado);
+         }          
+        } 
+        if (e.getSource()==btnlimpiar){
+         valor1.setText("");
+            valor2.setText("");
+            camporesultado.setText("");
+       } 
+        if(e.getSource()==btnsalir)
+        {
+         System.exit(0);   
+        }
     }
+     
+    public static void main(String[] args) {
+      JFrame.setDefaultLookAndFeelDecorated(true);
+      calculator calc = new calculator();
+      calc.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+      
+  
+    
 }
+
+
+
